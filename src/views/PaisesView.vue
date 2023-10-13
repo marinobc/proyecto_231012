2151fad01e2f0f1ec0f89af1f341b382e7934e37
marino barroso <marinobarroso2000@gmail.com>
2023-10-12 20:56:34 -0400

    Avance

---
+++ src/views/PaisesView.vue
@@ -0,0 +1,33 @@
+<template>
+    <p>{{ message }}</p>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+    setup(){
+        const message = ref ('');
+
+        const paises =async () => {
+            try {
+                const response = await axios.get(`api/v1/country`);
+                message.value = response.data;
+                console.log(message.value);
+            } catch (error) {
+                console.error("Hubo un error: ", error.message);
+            } 
+        }
+
+        onMounted(() => {
+            paises();
+        });
+
+        return {
+            message
+        };
+
+
+    }
+});
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 8888,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
   }
 })
